1199223f835326ebe2fe84090f7f6d0135e1b2e0
gem4ik <gema44@yandex.ru>
2023-04-26 22:17:23 +0300

    typing props and showing incoming messages

--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import s from './FriendMessage.module.css'
+import {MessageType} from "../HW1";
 
 // создать тип вместо any и отобразить приходящие данные
-const FriendMessage = (props: any) => {
+export type MessagePropsType = {
+    message: MessageType
+}
+const FriendMessage = (props: MessagePropsType) => {
     return (
         <div
             id={'hw1-friend-message-' + props.message.id}
@@ -11,8 +15,9 @@ const FriendMessage = (props: any) => {
             <div className={s.friendImageAndText}>
                 <img
                     id={'hw1-friend-avatar-' + props.message.id}
+                    alt={"avatar"}
                     // создаёт студент
-
+                    src={props.message.user.avatar}
                     //
                 />
                 <div className={s.friendText}>
@@ -21,7 +26,7 @@ const FriendMessage = (props: any) => {
                         className={s.friendName}
                     >
                         {/*создаёт студент*/}
-
+                        src={props.message.user.name}
                         {/**/}
                     </div>
                     <pre
@@ -29,7 +34,7 @@ const FriendMessage = (props: any) => {
                         className={s.friendMessageText}
                     >
                         {/*создаёт студент*/}
-
+                        src={props.message.message.text}
                         {/**/}
                     </pre>
                 </div>
@@ -39,7 +44,7 @@ const FriendMessage = (props: any) => {
                 className={s.friendTime}
             >
                 {/*создаёт студент*/}
-
+                src={props.message.message.time}
                 {/**/}
             </div>
         </div>
