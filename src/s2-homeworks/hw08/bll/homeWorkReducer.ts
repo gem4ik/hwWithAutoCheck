9585fafd2b7005c07b04b4a924b86c135e653505
gem4ik <mgemskij@gmail.com>
2023-06-04 19:18:19 +0300

    fix hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,25 +7,32 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            const newState = [...state]
-            if (action.payload === 'up') {
-                newState.sort(function (a, b) {
-                    if (a.age < b.age) {
-                        return 1
-                    } else return -41
-                })
-            }
-            if (action.payload === 'down') {
-                newState.sort(function (a, b) {
-                    if (a.age > b.age) {
-                        return 1
-                    } else return -41
-                })
-            }
-            return newState
+            // const newState = [...state]
+            // if (action.payload === 'up') {
+            //     newState.sort(function (a, b) {
+            //         if (a.age < b.age) {
+            //             return 1
+            //         } else return -41
+            //     })
+            // }
+            // if (action.payload === 'down') {
+            //     newState.sort(function (a, b) {
+            //         if (a.age > b.age) {
+            //             return 1
+            //         } else return -41
+            //     })
+            // }
+            // return newState
+            const newState = [...state].sort((a, b) => {
+                if(a > b) return 1
+                else if (a < b) return  -1
+                else return  0
+            })
+
+            return  action.payload === 'up' ? newState : newState.reverse()
         }
         case 'check': {
-            return state.filter(u=>u.age > 18)
+            return state.filter(u => u.age > action.payload)
 
         }
         default:
