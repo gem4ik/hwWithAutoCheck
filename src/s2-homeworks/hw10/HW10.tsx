366efa62391fe22953747d8a0a238eb8482d58c6
gem4ik <mgemskij@gmail.com>
2023-06-15 15:34:55 +0300

    hw10 not completed

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React, {useReducer} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import {AppStoreType} from './bll/store'
-import {loadingAC} from './bll/loadingReducer'
+import store, {AppStoreType} from './bll/store'
+import {loadingAC, loadingReducer} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
 
 /*
-* 1 - в файле loadingReducer.ts дописать типы и логику
+* 1 - в файле loadingReducer.ts дописать типы и логику +++
 * 2 - получить isLoading из редакса
 * 3 - дописать функцию setLoading
 * 4 - сделать стили в соответствии с дизайном
 * */
-
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
+
     const isLoading = false
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
